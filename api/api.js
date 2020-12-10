5e97f9690f1036dfcc5de06f9b19678b776dbbcf
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-10 18:09:52 -0500

    Started the users API endpoint

--- api/api.js
+++ api/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const userRouter = require('./users');
 
 const apiRouter = express.Router();
+apiRouter.use('/user', userRouter);
 
 module.exports = apiRouter;
---
+++ api/users.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const dbConnection = require('../sql/sql');
+
+const userRouter = express.Router();
+
+module.exports = userRouter;
