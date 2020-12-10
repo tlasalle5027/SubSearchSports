6b78ee962e7f0d489da34723f03d6a161a02c735
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-10 18:18:02 -0500

    Wrote query to get all users

--- api/users.js
+++ api/users.js
@@ -3,4 +3,19 @@ const dbConnection = require('../sql/sql');
 
 const userRouter = express.Router();
 
+userRouter.get('/', (req, res, next) => {
+    dbConnection.getConnection(function(err, conn) {
+        if(err){
+            res.sendStatus(404);
+        } else {
+            // Do something with the connection
+            conn.query("SELECT * FROM Users", function(err, rows, fields){
+                res.status(200).send(rows);
+            });
+            // Don't forget to release the connection when finished!
+            dbConnection.releaseConnection(conn);
+        }
+     });
+});
+
 module.exports = userRouter;
