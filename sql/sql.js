fe73bb80bd49412dcda3e0f55dd123bbd1723de8
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-10 16:39:40 -0500

    Added additional dependencies

--- sql/sql.js
+++ sql/sql.js
@@ -2,9 +2,9 @@ const mySQL = require('mysql2');
 
 const connection = mySQL.createConnection({
     host: "46.17.175.35",
-    user: "u654695621_jajy",
-    password: "meMaNeVeVy",
-    database: "u654695621_jajy",
+    user: "u654695621_subadmin",
+    password: "$u8A0M!n",
+    database: "u654695621_SubSearchSport",
     port: 3306
 });
 
