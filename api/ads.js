b0e30c88f693870fa00714faeadf462fb9a663a4
tlasalle5027 <tlasalle5027@gmail.com>
2020-12-17 12:24:14 -0500

    Initial setup for Ads API

---
+++ api/ads.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const dbConnection = require('../sql/sql');
+
+const adRouter = express.Router();
+
+module.exports = adRouter;
--- api/api.js
+++ api/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userRouter = require('./users');
+const adRouter = require('./ads');
 
 const apiRouter = express.Router();
 apiRouter.use('/user', userRouter);
+apiRouter.use('/ad', adRouter);
 
 module.exports = apiRouter;
