36236b040b1a74d4610313d507ff9602fa1c20fa
tlasalle5027 <tlasalle5027@gmail.com>
2021-01-28 12:42:04 -0500

    Front End Restart

--- client/components/AdList/AdList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Ad from '../Ads/Ads';
-
-const AdList = () => {
-    return (<Ad />);
-};
-
-export default AdList;
