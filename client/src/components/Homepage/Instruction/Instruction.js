3aad8ac71c28a550e8b7558f8fe6424032e4ed56
tlasalle5027 <tlasalle5027@gmail.com>
2021-02-01 13:37:59 -0500

    Used a table to align instructions better

--- client/src/components/Homepage/Instruction/Instruction.js
+++ client/src/components/Homepage/Instruction/Instruction.js
@@ -6,14 +6,18 @@ class Instruction extends React.Component{
         return (
             <section>
                 <section className="instruction">
-                    <num className="numberCircle">1</num><h1>Register</h1>
-                    <num className="numberCircle">2</num><h1>Post/Respond</h1>
-                    <num className="numberCircle">3</num><h1>PLAY!</h1>
-                </section>
-                <section className="instruction">
-                    <p>Register an account and fill out your profile</p>
-                    <p>Post an Ad looking for players, or respond to one</p>
-                    <p>Join your new teammates and get out and PLAY!</p>                    
+                    <table className="center">
+                        <tr>
+                            <td><num className="numberCircle">1</num><h1>Register</h1></td>
+                            <td><num className="numberCircle">2</num><h1>Post/Respond</h1></td>
+                            <td><num className="numberCircle">3</num><h1>PLAY!</h1></td>
+                        </tr>
+                        <tr>
+                            <td><p>Register an account and fill out your profile</p></td>
+                            <td><p>Post an Ad looking for players, or respond to one</p></td>
+                            <td><p>Join your new teammates and get out and PLAY!</p></td>
+                        </tr>
+                    </table>
                 </section>
             </section>            
             
