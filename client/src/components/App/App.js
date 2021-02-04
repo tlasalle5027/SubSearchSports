66b6a19c344dad820182f8fea996c4ae25add360
tlasalle5027 <tlasalle5027@gmail.com>
2021-02-04 09:26:52 -0500

    Fixed Header/Footer issues Completed boilerplate Homepage

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
+
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import Homepage from '../Homepage/Homepage';
 
 class App extends React.Component{
   render(){
     return(
-      <Homepage />
+      <body>
+        <Header />
+        <Homepage />
+        <Footer />
+      </body>
     )
   }
 }
--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './Homepage.css';
 
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 import Instruction from './Instruction/Instruction';
 
 class Homepage extends React.Component{
 
     render(){
         return (
-            <body>
-                <Header />
+            <body>            
                 <section className="intro">
                     <h1>Welcome to Sub Search Sport!</h1>
                     <p>Getting started with our system is as easy as...</p>
                     <Instruction />
                 </section>
-                <Footer />
+                <section className="userCount">
+                    <p>We have about <h1>0</h1> users </p>
+                    <p>Looking to play or looking for players!</p>
+                    <p>In about <h1>15</h1> different sports</p>
+                </section>
+                <section>
+                    <h1>JOIN US TODAY! GET OUT AND PLAY!</h1>
+                    <button className="logInButton">Log In/Sign Up</button>
+                </section>                               
             </body>
         );
     }
