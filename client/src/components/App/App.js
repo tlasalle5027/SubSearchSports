feb9d124c6559c1a026a26c91841de7431cea120
tlasalle5027 <tlasalle5027@gmail.com>
2021-02-04 15:09:36 -0500

    Added Header Links (Routing)

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -13,12 +13,10 @@ class App extends React.Component{
     return(
       <body>
         <Header />
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Homepage} />
-              <Route exact path="/ads" component={AdsPage} />
-            </Switch>
-          </Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/ads" component={AdsPage} />
+        </Switch>          
         <Footer />
       </body>
     )
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -14,18 +14,18 @@ class Header extends React.Component {
     render(){
         return (
             <nav class="navigation-bar">
-                <img alt="Sub Search Sport Logo" class="logo" src={logo} />
+                <a href="/"><img alt="Sub Search Sport Logo" class="logo" src={logo} /></a>
                 <ul>
                     <li><SearchBar /></li>
-                    <li>Ads</li>
+                    <li><a href="/ads">Ads</a></li>
                     <li>Users</li>
                     <li>{this.state.loggedin ? 'Messages' : ''}</li>
                     <li><button className="logInButton">{this.state.loggedIn ? 'Log Out' : 'Log In/Sign Up'}</button></li>
                 </ul>
             </nav>
+            
         );
     }
-
 }
 
 export default Header;
