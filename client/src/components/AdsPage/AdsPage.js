a1b7d272c654cec10dee49b43f188e6cc4f1cce7
tlasalle5027 <tlasalle5027@gmail.com>
2021-02-04 14:36:50 -0500

    Created an Ads Page Set up basic Routing

---
+++ client/src/components/AdsPage/AdsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AdsPage.css';
+
+class AdsPage extends React.Component{
+
+
+    render(){
+        return(
+            <body>
+                <p>AdsPage</p>
+            </body>
+        );
+    }
+}
+
+export default AdsPage;
--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+
 import Homepage from '../Homepage/Homepage';
+import AdsPage from '../AdsPage/AdsPage';
 
 class App extends React.Component{
   render(){
     return(
       <body>
         <Header />
-        <Homepage />
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/ads" component={AdsPage} />
+            </Switch>
+          </Router>
         <Footer />
       </body>
     )
