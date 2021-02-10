66af15e921c22d4f715a66445a04cca1edf77071
tlasalle5027 <tlasalle5027@gmail.com>
2021-02-10 16:08:38 -0500

    Header Mobile  CSS

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,18 +8,17 @@ class Header extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = { loggedIn: false };
+        this.state = { loggedIn: true };
     }
 
     render(){
         return (
             <nav className="navigation-bar">
                 <a href="/"><img alt="Sub Search Sport Logo" className="logo" src={logo} /></a>
-                <ul>
-                    <li><SearchBar /></li>
+                <span className="SearchBar"><SearchBar /></span>
+                <ul>                    
                     <li><a href="/ads">Ads</a></li>
                     <li>Users</li>
-                    <li>{this.state.loggedin ? 'Messages' : ''}</li>
                     <li><button className="logInButton">{this.state.loggedIn ? 'Log Out' : 'Log In/Sign Up'}</button></li>
                 </ul>
             </nav>
