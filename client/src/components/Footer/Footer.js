cebbda67f044575be9e5cc46b3ad69119b45e9f4
tlasalle5027 <tlasalle5027@gmail.com>
2021-02-19 14:07:14 -0500

    Added background-img randomizer to Header

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ class Footer extends React.Component{
             <footer>
                 <p>&copy; 2021 <a href="http://www.cfmsoftware.com/">CFM Software</a></p>
                 <p>Icons courtesy of <a href="https://icons8.com">Icons8</a></p>
+                <p>Football Field Header vector created by<a href="https://www.freepik.com/vectors/background"> macrovector - www.freepik.com</a></p>
             </footer>            
         );
     }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import logo from '../../resources/img/SSS_Logo_Transparent.png';
 import './Header.css';
 
 import SearchBar from '../SearchBar/SearchBar';
 
+import logo from '../../resources/img/SSS_Logo_Transparent.png';
+import headerBaseball from '../../resources/img/Header/baseball-diamond-37200.png';
+import headerHockey from '../../resources/img/Header/hockey-rink.jpg';
+import headerSoccer from '../../resources/img/Header/soccer-field.png';
+import headerFootball from '../../resources/img/Header/football-field.jpg';
+
 class Header extends React.Component {
     constructor(props){
-        super(props);
+        super(props);        
 
         this.state = { loggedIn: false };
+    }    
+
+    componentDidMount(){
+        const bgImages = [headerBaseball, headerHockey, headerSoccer, headerFootball];
+        const random = Math.floor(Math.random() * bgImages.length);
+        this.setState({bgImage: bgImages[random]});
     }
 
     render(){
         return (
-            <nav className="navigation-bar">
+            <nav className="navigation-bar" style={{backgroundImage: `url(${this.state.bgImage})`}}>
                 <a href="/"><img alt="Sub Search Sport Logo" className="logo" src={logo} /></a>
                 <span className="SearchBar"><SearchBar /></span>
                 <ul>                    
@@ -21,8 +32,7 @@ class Header extends React.Component {
                     <li><a href="/users">Users</a></li>
                     <li><a href="/login"><button className="logInButton">{this.state.loggedIn ? 'Log Out' : 'Log In/Sign Up'}</button></a></li>
                 </ul>
-            </nav>
-            
+            </nav>            
         );
     }
 }
